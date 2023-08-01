8ea34e6bf53fb97ef0d566c63e4eb6decdfb48b6
Aleksandra Janke <aleksandra.janke11@gmail.com>
2023-08-01 14:47:00 +0100

    my changes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var age = document.getElementById("age");
-var height = document.getElementById("height");
-var weight = document.getElementById("weight");
-var male = document.getElementById("m");
-var female = document.getElementById("f");
-var form = document.getElementById("form");
-var resultArea = document.getElementById(".comment");
+
+let height = document.getElementById("height");
+let weight = document.getElementById("weight");
+let male = document.getElementById("m");
+let female = document.getElementById("f");
+let form = document.getElementById("form");
+let resultArea = document.getElementById(".comment");
 
 
 function calculate(){
-    if (age.value== '' || height.value== '' || weight.value== '' || (male.checked==false && female.checked==false)){
+    if (height.value== '' || weight.value== '' || (male.checked==false && female.checked==false)){
         console.log('All fields are required!')
     }else {
         countBMI()
@@ -16,18 +16,10 @@ function calculate(){
 }
 
 function countBMI(){
-    var p = [age.value, height.value, weight.value];
 
-    if(male.checked){
-        p.push("male");
-    }else if(female.checked){
-        p.push("female")
-    }
-
-console.log(p)
-var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
-console.log(bmi)
-var result = '';
+let bmi = weight.value/(height.value/100*height.value/100)
+console.log(bmi);
+let result
 if(bmi<18.5){
     result = 'underweight';
 }else if(18.5<=bmi&&bmi<=24.9){
@@ -36,7 +28,7 @@ if(bmi<18.5){
     result = 'overweight';
 }else if(30<=bmi&&bmi<=34.9){
     result = 'obese'
-}else if(35<=bmi){
+}else {
     result = 'extremely obese';
 }
 console.log(result)
